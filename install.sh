e562af162791ba59e5d0becf641103c53246b2b6
CA-Team <ca-team@gisloanmac128.ca.com>
2017-06-01 15:48:30 -0700

    Omar fix on password special chars

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
 set -o allexport +x
 source $DIR/qube_common_functions.sh
+get_options $@ > /dev/null
 eval $(get_options $@)
 if [ "$return_code" -eq 1 ]; then
     exit $return_code
