07e294952c66de4f2caac9ed6b3eb059f8989b71
Hyunji Kim <hj.eunice@gmail.com>
2017-06-13 13:01:37 -0700

    updated run.sh

--- run.sh
+++ run.sh
@@ -2,18 +2,30 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
 
+# evaluate arguments: qubebuilder's credentials are required
 set -o allexport
 source $DIR/qube_common_functions.sh
+get_options $@ > /dev/null
 eval $(get_options $@)
-echo $resolved_args
+if [ "$return_code" -eq 1 ]; then
+    exit $return_code
+fi
+if [ "$no_args" -eq 1 ]; then
+    show_help;
+    exit 1
+fi
 
 if [ $verbose ]; then
     set -x
 fi
 
-if [ -e .client_env ]; then
-    source .client_env
+# export environment variables
+# .client_env must exist!
+if [ ! -f .client_env ]; then
+    echo "Cannot run Qubeship: file .client_env does not exist"
+    exit 1
 fi
+source .client_env
 
 if [ -e $SCM_CONFIG_FILE ] ; then
     source $SCM_CONFIG_FILE
@@ -27,6 +39,7 @@ if [ -e ~/.qube_cli_profile ]; then
     source ~/.qube_cli_profile
 fi
 
+# set permissions of docker socket
 if [ $is_osx ]; then
     if [ "$DOCKER_INSTALL_TYPE" == "mac" ] ; then
         chmod a+rwx /var/run/docker.sock
@@ -46,11 +59,14 @@ if [ $is_beta ]; then
     export LISTENER_URL=$NGROK_HOSTNAME
 fi
 
-echo "LISTENER URL is : $LISTENER_URL"
+echo "LISTENER URL is: $LISTENER_URL"
 docker-compose pull cli
 echo "starting $base_command $files $options"
 $base_command $files $options
 
+# as qube-consul runs in development mode, it won't persist any data(tenant info)
+# therefore, the qubebuilder should always log in to the system to populate the tenant data
+# this will be gone after the tenant API is re-written to use different database
 extra_args=""
 if [ ! -z $github_username ]; then
     extra_args="--username $github_username --password $github_password --organization $SYSTEM_GITHUB_ORG --skip-defaults"
